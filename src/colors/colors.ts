052144680aed7916f4d27af72b21a5bbf7cdb112
Muhammad Hassan <hassanofficial2k3@gmail.com>
2023-06-26 14:50:52 +0500

    get homeui code from hamid/feat/home_ui and fix issues

--- src/colors/colors.ts
+++ src/colors/colors.ts
@@ -1,15 +1,22 @@
-const SUN_FLOWER = '#f1c40f';
+const YELLOW = '#e69b00';
 const ASBESTOS = '#7f8c8d';
 const MIDNIGHT_BLUE = '#2c3e50';
 const EMERALD = '#2ecc71';
 const ALIZARIN = '#e74c3c';
 const CLOUDS = '#ecf0f1';
 const SILVER = '#bdc3c7';
+const WHITE = '#fff';
+const BLACK = '#000';
+const LIGHTGRY = '#ffFFFF26';
+const LIGHTCONTENT = 'light-content';
+const DARKCONTENT = 'dark-content';
 
 const common: Record<string, string> = {
-  PRIMARY: SUN_FLOWER,
+  PRIMARY: YELLOW,
   SUCCESS: EMERALD,
   ERROR: ALIZARIN,
+  SEARCHBARCONTAINER: LIGHTGRY,
+  LAUNCHSCREEN: YELLOW,
 };
 
 type Colors = {
@@ -23,12 +30,16 @@ const colors: Colors = {
     BACKGROUND: CLOUDS,
     TEXT: MIDNIGHT_BLUE,
     TEXT_SECONDARY: ASBESTOS,
+    SEARCHBAR: BLACK,
+    STATUSBAR: DARKCONTENT,
   },
   dark: {
     ...common,
     BACKGROUND: MIDNIGHT_BLUE,
     TEXT: CLOUDS,
     TEXT_SECONDARY: SILVER,
+    SEARCHBAR: WHITE,
+    STATUSBAR: LIGHTCONTENT,
   },
 };
 
